c2ea879b090447046462cfdf0c55294c29ff2a4f
jd <duverseaujohnny21@gmail.com>
2020-10-30 13:29:39 -0400

    create files, add class and code

---
+++ LocalBusiness/Models/Shop.cs
@@ -0,0 +1,13 @@
+namespace LocalBusiness.Models
+{
+  public class Shop
+  {
+    public int ShopId { get; set; }
+    public string Name {get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public  string  Types{ get; set; }
+    public string city { get; set; }
+    public string rates { get; set; }
+  }
+}
