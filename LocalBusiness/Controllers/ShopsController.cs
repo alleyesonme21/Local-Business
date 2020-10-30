3fe419219b24d36bbba83114161d6fa2c62298f5
jd <duverseaujohnny21@gmail.com>
2020-10-30 18:48:01 -0400

    modified HttpGet and add code

--- LocalBusiness/Controllers/ShopsController.cs
+++ LocalBusiness/Controllers/ShopsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
@@ -16,15 +17,67 @@ namespace LocalBusiness.Controllers
     }
     //GET /api/shops
     [HttpGet]
-    public ActionResult<IEnumerable<Shop>> Get()
+    public ActionResult<IEnumerable<Shop>> Get( string name, string address, string phone, string types, string city, string rate)
     {
-      return _db.Shops.ToList();
+      var query = _db.Shops.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(x => x.Name == name);
+      }
+      
+      if (address != null)
+      {
+        query = query.Where(x => x.Address == address);
+      }
+
+      if (phone != null)
+      {
+        query = query.Where(x => x.Phone == phone);
+      }
+
+      if (types != null)
+      {
+        query = query.Where(x => x.Types == types);
+      }
+
+      if (city != null)
+      {
+        query = query.Where(x => x.city == city);
+      }
+      if (rate != null)
+      {
+        query = query.Where(x => x.rates == rate);
+      }
+  
+      return query.ToList();
     }
     //POST api/shops
     [HttpPost]
     public void Post([FromBody] Shop shop)
     {
       _db.Shops.Add(shop);
+      _db.SaveChanges();
+    }
+    [HttpGet("{id}")]
+    public ActionResult<Shop> Get(int id)
+    {
+      return _db.Shops.FirstOrDefault(x => x.ShopId == id);
+    }
+    //PUT /api/shops
+    [HttpPut("id")]
+    public void Put(int id, [FromBody] Shop shop)
+    {
+      shop.ShopId = id;
+      _db.Entry(shop).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+    //DELETE / api/shops/4
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var shop = _db.Shops.FirstOrDefault(x => x.ShopId == id);
+      _db.Shops.Remove(shop);
       _db.SaveChanges();
     }
   }
