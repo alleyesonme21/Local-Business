746efe1849f2b4104fe1d8ac21917881c37335d3
jd <duverseaujohnny21@gmail.com>
2020-10-30 13:30:00 -0400

    create files, add class and code

---
+++ LocalBusiness/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+namespace LocalBusiness.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name {get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public  string  Cuisine { get; set; }
+    public string city { get; set; }
+    public int rates { get; set; }
+
+  }
+}
